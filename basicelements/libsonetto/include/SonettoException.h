633db23755492797b8aede71fa3a34593fe1932d
Guilherme Vieira <super.driver.512@gmail.com>
2008-12-18 08:58:43 +0000

    Creating basicelements branch
    git-svn-id: http://sonetto.googlecode.com/svn@189 0bea0c47-0c44-0410-8422-6f9b8df7ced0

---
+++ basicelements/libsonetto/include/SonettoException.h
@@ -0,0 +1,71 @@
+/*-----------------------------------------------------------------------------
+Copyright (c) 2009, Sonetto Project Developers
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1.  Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+2.  Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+3.  Neither the name of the Sonetto Project nor the names of its contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+-----------------------------------------------------------------------------*/
+
+#ifndef SONETTO_EXCEPTION_H
+#define SONETTO_EXCEPTION_H
+
+#include <string>
+#include <exception>
+
+namespace Sonetto
+{
+    /** Sonetto Exception
+
+        Carries information about an error that ocurred during Sonetto execution.
+    */
+    class Exception : public std::exception
+    {
+    public:
+        /** Default constructor
+
+        @remarks
+            Do not instantiate exceptions by yourself. Use the SONETTO_THROW
+            macro instead.
+        @param desc
+            Error description.
+        @param aFile
+            File from which the exception was thrown.
+        @param line
+            Line from which the exception was thrown.
+        */
+        Exception(std::string desc,const char *aFile,size_t line);
+
+        /// Destructor
+        inline ~Exception() throw() {}
+
+        /// Gets formated error message
+        inline const char *what() const throw() { return mWhat.c_str(); }
+
+    private:
+        /// Formated error message
+        std::string mWhat;
+    };
+} // namespace
+
+#endif
